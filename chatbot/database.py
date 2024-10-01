8b9aeb496665e7b7a20398735daa9a11e8a2c348
zeberity123 <darudayu123@gmail.com>
2024-10-02 08:03:21 +0900

    update database.py

--- chatbot/database.py
+++ chatbot/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = 'sqlite:///fastapi.db'
+DB_URL = 'sqlite:///./fastapi.db'
 
-engine = create_engine(DB_URL, pool_size=50, connect_args={'check_same_thread': False})
+engine = create_engine(DB_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
