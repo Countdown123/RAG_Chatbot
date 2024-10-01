c2aef151f8d5cb17e1ba07f352e44895a02413b3
zeberity123 <darudayu123@gmail.com>
2024-10-02 08:04:34 +0900

    separatee user_crud.py

--- chatbot/user/user_crud.py
+++ chatbot/user/user_crud.py
@@ -1,6 +1,5 @@
 from sqlalchemy.orm import Session
-
-from models import User
+from models import User, UserCreate
 from user.user_schema import NewUserForm
 
 from passlib.context import CryptContext
@@ -24,4 +23,22 @@ def create_user(new_user: NewUserForm, db: Session):
     db.add(user)
     db.commit()
 
-        
+def get_user(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def create_user(db: Session, user: UserCreate):
+    hashed_password = pwd_context.hash(user.password)
+    db_user = User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user(db, email)
+    if user is None:
+        return False
+    if not pwd_context.verify(password, user.hashed_password):
+        return False
+    return user
